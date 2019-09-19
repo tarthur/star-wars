f61162e0ec079da944f50600fd45edcf73199d92
tarthur <terzi.arthur@gmail.com>
2019-09-20 00:22:03 +0300

    Updates

---
+++ precache-manifest.841016f7878dcb96f657e7011d6100c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c395518b9df0363b251d992376d6d0b3",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "5f5d9a40e6401275fb1b",
+    "url": "/star-wars/static/css/main.1b76c8e9.chunk.css"
+  },
+  {
+    "revision": "19e9e89a24278feba53d",
+    "url": "/star-wars/static/js/2.0153a9fb.chunk.js"
+  },
+  {
+    "revision": "5f5d9a40e6401275fb1b",
+    "url": "/star-wars/static/js/main.4a17e5b1.chunk.js"
+  },
+  {
+    "revision": "38bd5e4bd9b3c5a90141",
+    "url": "/star-wars/static/js/runtime~main.84c833ae.js"
+  },
+  {
+    "revision": "0cf7e1d82ef31810e99a733492310ca4",
+    "url": "/star-wars/static/media/logo.0cf7e1d8.png"
+  }
+]);
